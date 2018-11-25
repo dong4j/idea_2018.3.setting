f240067690ca8c345bccf4f026048c8fdb47e4a3
dong4j <dong4j@gmail.com>
2018-11-25 22:49:42 +0800

    IU-2018.3 <dong4j@dong4j.local Create markdown.xml, git_toolbox_2.xml, project.default.xml, diff.xml, Material Oceanic copy.icls, yiiguxing.translation.xml, Default.xml, file.template.settings.xml, ignore.xml, find.xml, File Header.java, code.style.schemes.xml, _@user_Material Oceanic.icls, _@user_Darcula.icls, path.macros.xml, mavenVersion.xml, javarebel.xml, React.xml, Default.xml, editor.codeinsight.xml, grepConsole.xml, keymap.xml, editor.xml, debugger.xml, updates.xml, baseRefactoring.xml, ide.general.xml, _@user_Default.icls, google-java-style.xml, CodeGlance.xml, lombok-plugin.xml, git.xml, terminal.xml, ui.lnf.xml, filetypes.xml, projectView.xml, javaeeExternalResources.xml, plugin_ui.xml, colors.scheme.xml, External Tools.xml, notifications.xml, databaseDrivers.xml, templates.xml, material_custom_theme.xml, Class.java, jdk.table.xml, mavenRunHelper.xml, acceptedLanguageLevels.xml, intentionSettings.xml, laf.xml, Mac OS X 10_5_ copy.xml, dong4j.xml, web-browsers.xml, material_theme.xml, proxy.settings.xml, vcs.xml, codota.xml, databaseSettings.xml, mybatis.xml, usageView.xml

---
+++ fileTemplates/includes/File Header.java
@@ -0,0 +1,20 @@
+##/**
+## * <p>Title: ${PACKAGE_NAME}</p>
+## * <p>Company: 成都返空汇网络技术有限公司</p>
+## * <p>Copyright © 2014 返空汇 All Rights Reserved</p>
+## * <p>Description: ${description}</p>
+## * author: dong4j
+## * emali: dongshijie@fkhwl.com
+## * version: 1.0 
+## * date: ${YEAR}年${MONTH}月${DAY}日 ${HOUR}点${MINUTE}分
+## * updatetime:
+## * reason:
+## */
+/**
+ * <p>Company: 科大讯飞股份有限公司-四川分公司</p>
+ * <p>Description: ${description}</p>
+ *
+ * @author     dong4j
+ * @date       ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}
+ * @email      sjdong3@iflytek.com
+ */
---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,13 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+##parse("File Header.java")
+
+/**
+ * <p>Company: 科大讯飞股份有限公司-四川分公司</p>
+ * <p>Description: ${description}</p>
+ *
+ * @author     dong4j
+ * @since      ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}
+ * @email      sjdong3@iflytek.com
+ */
+public class ${NAME} {
+}
